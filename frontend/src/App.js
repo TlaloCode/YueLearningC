10434eaf56b83225fd7ac3639202e0aea5bd07a5
yunue <aideromo10@gmail.com>
2025-03-17 21:21:30 -0600

    agregue vista detalle de curso(Aide)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,9 @@ import StudentProfile from "./paginas/StudentProfile";
 import TeacherCourses from "./paginas/TeacherCourses";
 import CreateCourse from "./paginas/CreateCourse";
 import StudentCourses from "./paginas/StudentCourses";
+import TeacherCourseDetail from "./paginas/TeacherCourseDetail";
+
+
 
 
 
@@ -36,8 +39,7 @@ const App = () => {
               <Route path="/mis-cursos" element={<TeacherCourses />} />
               <Route path="/crear-curso" element={<CreateCourse />} />
               <Route path="/mis-cursos-estudiante" element={<StudentCourses />} />
-
-
+              <Route path="/curso-detalle" element={<TeacherCourseDetail />} />
 
 
 
